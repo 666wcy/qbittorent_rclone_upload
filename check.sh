814d60f3fe180a2af6630a414c36b733381a80a5
666wcy <40867688+666wcy@users.noreply.github.com>
2020-10-25 17:08:36 +0800

    防止中断

--- check.sh
+++ check.sh
@@ -2,7 +2,9 @@
   file="/upload/qb_auto.sh"	
 	if [ -f "$file" ]	
 	then				
-	 echo "$file found."	
+	 echo "$file found."
+	 tail -f /dev/null
 	else				
 	 wget -P /upload https://github.com/666wcy/qbittorent_rclone_upload/raw/main/qb_auto.sh	
+  	 tail -f /dev/null
 	fi		
