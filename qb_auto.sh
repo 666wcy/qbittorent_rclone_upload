8dceec2f859f92db363fc700fff96327345255bc
666wcy <769020367@qq.com>
2020-10-31 21:52:39 +0800

    更新脚本

--- qb_auto.sh
+++ qb_auto.sh
@@ -6,17 +6,20 @@ save_dir=$4
 files_num=$5
 torrent_size=$6
 file_hash=$7
-the_dir="${save_dir//\/downloads\//}"
 
-qb_version="4.3.0.1"
-qb_username="admin"
-qb_password="adminadmin"
-qb_web_url="http://localhost:8080"
-leeching_mode="true"
-log_dir="/config/log"
-rclone_dest="yun"
-rclone_parallel="32"
-auto_del_flag="rclone"
+DRIVE_NAME='onedrive'   # 挂载盘名称
+DRIVE_PATH='/upload'		#上传到盘的地址，后面没有 /
+DOWNLOAD_PATH='/downloads/'		#qb下载默认地址
+
+RETRY_NUM=3		#rclone上传重试次数
+
+qb_version="4.3.0.1"  #qb版本
+qb_username=""        #qb用户名
+qb_password=""        #qb密码
+qb_web_url="http://localhost:8080"   #qb web地址
+leeching_mode="false"  #true为自动删除上传的种子
+log_dir="/config/log"  #日志保存地址
+auto_del_flag="test"   #上传完成后将种子标记的标签
 
 
 if [ ! -d ${log_dir} ]
@@ -25,6 +28,50 @@ then
 fi
 
 version=$(echo $qb_version | grep -P -o "([0-9]\.){2}[0-9]" | sed s/\\.//g)
+
+
+RED_FONT_PREFIX="\033[31m"
+LIGHT_GREEN_FONT_PREFIX="\033[1;32m"
+YELLOW_FONT_PREFIX="\033[1;33m"
+LIGHT_PURPLE_FONT_PREFIX="\033[1;35m"
+FONT_COLOR_SUFFIX="\033[0m"
+INFO="[${LIGHT_GREEN_FONT_PREFIX}INFO]"
+ERROR="[${RED_FONT_PREFIX}ERROR]"
+WARRING="[${YELLOW_FONT_PREFIX}WARRING]"
+
+
+
+
+
+function UPLOAD_FILE() {
+    echo -e "$(DATE_TIME) ${INFO} Start upload files..."
+    RETRY=0
+    while [ ${RETRY} -le ${RETRY_NUM} ]; do
+        [ ${RETRY} != 0 ] && (
+            echo
+            echo -e "$(DATE_TIME) ${ERROR} Upload failed! Retry ${RETRY}/${RETRY_NUM} ..."
+            echo
+        )
+        echo ${UPLOAD_PATH}
+        echo ${REMOTE_PATH}
+        rclone copy -v "${UPLOAD_PATH}" "${REMOTE_PATH}"
+        RCLONE_EXIT_CODE=$?
+        if [ ${RCLONE_EXIT_CODE} -eq 0 ]; then
+            
+            break
+        else
+            RETRY=$((${RETRY} + 1))
+            [ ${RETRY} -gt ${RETRY_NUM} ] && (
+                echo "$(DATE_TIME) ${ERROR} Upload failed: ${UPLOAD_PATH}"
+               
+            )
+            sleep 3
+        fi
+    done
+}
+
+
+
 
 function qb_login(){
 	if [ ${version} -gt 404 ]
@@ -80,15 +127,6 @@ function qb_del(){
 	fi
 }
 
-function rclone_copy(){
-	if [ ${type} == "file" ]
-	then
-		rclone_copy_cmd=$(rclone -v copy --transfers ${rclone_parallel} --log-file  ${log_dir}/qbauto_copy.log "${content_dir}" ${rclone_dest}:qbauto/${the_dir})
-	elif [ ${type} == "dir" ]
-	then
-		rclone_copy_cmd=$(rclone -v copy --transfers ${rclone_parallel} --log-file ${log_dir}/qbauto_copy.log "${content_dir}"/ ${rclone_dest}:qbauto/${the_dir}"${torrent_name}")
-	fi
-}
 
 function qb_add_auto_del_tags(){
 	if [ ${qb_v} == "1" ]
@@ -102,23 +140,47 @@ function qb_add_auto_del_tags(){
 	fi
 }
 
+echo ${content_dir}
 if [ -f "${content_dir}" ]
 then
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] 类型：文件"
    echo "[$(date '+%Y-%m-%d %H:%M:%S')] 类型：文件" >> ${log_dir}/qb.log
    type="file"
-   rclone_copy
+   
+    
+    
+    RELATIVE_PATH=${content_dir#${DOWNLOAD_PATH}}  #文件夹名称
+    
+    echo $RELATIVE_PATH
+    NEW_PATH=${RELATIVE_PATH%%/*}
+    echo $NEW_PATH
+    UPLOAD_PATH="${content_dir}"
+    REMOTE_PATH="${DRIVE_NAME}:${DRIVE_PATH}/${NEW_PATH}"
+    echo $UPLOAD_PATH
+    echo $REMOTE_PATH
+   UPLOAD_FILE
    qb_login
    qb_add_auto_del_tags
-   #qb_del
+   qb_del
 elif [ -d "${content_dir}" ]
 then 
+   echo "[$(date '+%Y-%m-%d %H:%M:%S')] 类型：目录"
    echo "[$(date '+%Y-%m-%d %H:%M:%S')] 类型：目录" >> ${log_dir}/qb.log
    type="dir"
-   rclone_copy
+   RELATIVE_PATH=${content_dir#${DOWNLOAD_PATH}}  #文件夹名称
+    
+    echo $RELATIVE_PATH
+    UPLOAD_PATH="${content_dir}"
+    REMOTE_PATH="${DRIVE_NAME}:${DRIVE_PATH}/${RELATIVE_PATH}"
+
+    echo $UPLOAD_PATH
+    echo $REMOTE_PATH
+   UPLOAD_FILE
    qb_login
    qb_add_auto_del_tags
-   #qb_del
+   qb_del
 else
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] 未知类型，取消上传"
    echo "[$(date '+%Y-%m-%d %H:%M:%S')] 未知类型，取消上传" >> ${log_dir}/qb.log
 fi
 
@@ -129,6 +191,9 @@ echo "保存路径：${save_dir}" >> ${log_dir}/qb.log
 echo "文件数：${files_num}" >> ${log_dir}/qb.log
 echo "文件大小：${torrent_size}Bytes" >> ${log_dir}/qb.log
 echo "HASH:${file_hash}" >> ${log_dir}/qb.log
-echo "Cookie:${cookie}" >> ${log_dir}/qb.log
+echo "上传地址:${UPLOAD_PATH}" >> ${log_dir}/qb.log
+#echo "Cookie:${cookie}" >> ${log_dir}/qb.log
 echo -e "-------------------------------------------------------------\n" >> ${log_dir}/qb.log
+
+
 
